d4a9c27dffeeb89f124a458f058acf81fa8a5469
the creator <chenxi.yan@gmail.com>
2024-02-18 01:50:55 -0500

    progress: cont'd

---
+++ src/app/api/topics/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/db/config";
+import Topics, { ITopics } from "@/models/topics";
+
+export async function POST(request: NextRequest) {
+  try {
+    connectDB();
+
+    const submission: ITopics = await request.json();
+
+    // Validate input data
+    if (!submission.email || !submission.topic) {
+      throw new Error("Email and topic are required fields");
+    }
+
+    // Save the topic to the database
+    const topic = await new Topics(submission).save();
+
+    return NextResponse.json(
+      {
+        message: `${topic.topic} created successfully!`,
+      },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    console.error("Error processing POST request:", error);
+
+    return NextResponse.json(
+      { error: "An unexpected error occurred while processing your request" },
+      { status: 400 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    connectDB();
+
+    let topics = await Topics.find({});
+
+    return NextResponse.json(topics, { status: 200 });
+  } catch (error: any) {
+    console.error("Error processing GET request:", error);
+
+    return NextResponse.json(
+      { error: "An unexpected error occurred while processing your request" },
+      { status: 400 }
+    );
+  }
+}
--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -2,9 +2,59 @@
 
 import Image from "next/image";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { useEffect, useState } from "react";
+import AddPopup from "@/components/AddPopup";
+import "./Settings.css"
 
 export default function Settings() {
   const { user, error, isLoading } = useUser();
+  const [showPopup, setShowPopup] = useState(false);
+  const [topics, setTopics] = useState([]);
+
+  const openPopup = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
+  const handleAddTopic = async (topicData: any) => {
+    // Send a request to your API to add the topic
+    try {
+      const response = await fetch("/api/topics", {
+        method: "POST",
+        body: JSON.stringify(topicData),
+      });
+      if (response.ok) {
+        console.log("added successfully");
+      } else {
+        const errorData = await response.json();
+        console.error("Error:", errorData);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    let fetchData = async () => {
+      try {
+        let response = await fetch("/api/topics", {
+          method: "GET",
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setTopics(data);
+        } else {
+          throw new Error("Error fetching topics");
+        }
+      } catch (error) {
+        console.error("Error fetching topics:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
@@ -31,6 +81,34 @@ export default function Settings() {
           <button>
             <a href="/api/auth/logout">Logout</a>
           </button>
+        </div>
+      </div>
+      <button onClick={openPopup}>Add Topic</button>
+
+      {/* Render popup if showPopup is true */}
+      {showPopup && (
+        <AddPopup
+          userEmail={user.email ?? ""}
+          onClose={closePopup}
+          onSubmit={handleAddTopic}
+        />
+      )}
+
+      <div
+        className="flex flex-col items-center justify-center mt-10 border border-gray-300 p-4 rounded style"
+        style={{
+          backgroundColor: "#7E9BF6",
+          width: "50%",
+          margin: "0 auto",
+          borderRadius: "50px",
+        }}
+      >
+        <div className="dashboard grid grid-cols-2 max-sm:grid-cols-1 topics">
+          {topics.map((topic: { topic: string }, index: number) => (
+            <div key={index} className="px-4 flex items-center">
+              <span className="text-xl">{topic.topic}</span>
+            </div>
+          ))}
         </div>
       </div>
     </div>
